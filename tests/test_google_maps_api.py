55a1f3e59769e0fce7709d93c9f816f7fb506dc8
Oleksandr-Rosenko <113106385+Oleksandr-Rosenko@users.noreply.github.com>
2022-11-07 12:01:29 +0200

    43_leesson_test

---
+++ tests/test_google_maps_api.py
@@ -0,0 +1,12 @@
+from requests import Response
+
+from utils.api import Google_maps_api
+
+
+"""Creating,edidting,deleteing location"""
+class test_create_place():
+
+    def test_create_new_place(self):
+
+        print("POST method")
+        result_post: Response = Google_maps_api.create_new_place()
---
+++ utils/api.py
@@ -0,0 +1,35 @@
+from  utils.https_method import http_methods
+
+"""Metods  for testing google maps API"""
+
+base_url = "https://rahulshettyacademy.com"
+key = "?key=qaclick123"
+
+
+class Google_maps_api():
+    @staticmethod
+    def create_new_place():
+
+        json_for_create_new_place = {
+            "location":{
+            "lat":-38.383494,
+            "lng":33.427362
+            },"accuracy":50,
+            "name":"Front line house",
+            "phone_number": "(+91)9838933937",
+            "address": "29,side layout,cohen 09",
+            "types": [
+                "shoe park",
+                "shop"
+            ],
+            "website": "http://google.com",
+            "language": "French-IN"
+        }
+        post_resource = "/maps/api/place/add/json" #resource of post method
+        post_url = base_url + post_resource + key
+        print(post_url)
+        result_post = http_methods.post(post_url, json_for_create_new_place)
+        print(result_post.text)
+        return result_post
+
+
