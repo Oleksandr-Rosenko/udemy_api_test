2df94a9a575f10f9f809c5401e468e0f96cd8149
Oleksandr-Rosenko <rosenko29@gmail.com>
2022-11-12 14:02:36 +0200

    Method POST and GET success tested

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -7,7 +7,13 @@ class Test_create_place():
 
     def test_create_new_place(self):
 
-        print("POST method")
+        print("Method POST")
         result_post: Response = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+
+        print("Method GET")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+
 
 
--- utils/api.py
+++ utils/api.py
@@ -31,3 +31,16 @@ class Google_maps_api():
         result_post = Http_methods.post(post_url, json_for_create_new_place)
         print(result_post.text)
         return result_post
+
+    """Method for checking  creating new location """
+
+    @staticmethod
+    def get_new_place(place_id):
+
+        get_resource = "/maps/api/place/get/json"
+        get_url = base_url + get_resource + key + "&place_id=" + place_id
+        print(get_url)
+        result_get = Http_methods.get(get_url)
+        print(result_get.text)
+        return result_get
+
